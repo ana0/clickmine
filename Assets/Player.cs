a56ac1997a7760d86754b858e5d75506d3ee7255
ana0 <sarah.anne.friend@gmail.com>
2017-06-04 18:18:41 -0400

    Player movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    private Vector3 speed = new Vector3(0, 0, -1f);
+    public float speed = -10f;
+    public float momentum = 10f;
+    private Vector3 movement;
+    Rigidbody playerRigidbody;
 
-	// Use this for initialization
-	void Start () {
-
-	}
+    // Use this for initialization
+    void Awake () {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        this.transform.Translate(speed*Time.deltaTime);
+        //this.transform.Translate(new Vector3(0f, 0f, speed) * Time.deltaTime);
 	}
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Move(h, v);
+    }
+
+    public void Move(float h, float v)
+    {
+        //Set movement vector from keyboard input, speed is our constant forward float
+        movement.Set(-h, v, speed);
+        movement = movement * momentum * Time.deltaTime;
+        //Add movement vector to the player's current transform
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+
 }
