67d0b6f807cf51950dbee91011c1cf7d5188f1e4
ana0 <sarah.anne.friend@gmail.com>
2017-06-04 15:08:35 -0400

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Vector3 speed = new Vector3(0, 0, -1f);
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(speed*Time.deltaTime);
+	}
+}
