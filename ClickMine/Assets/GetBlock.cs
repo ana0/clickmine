277d08f201d0a27bcd68890e4b7d5d64f2b506fd
ana0 <sarah.anne.friend#gmail.com>
2017-10-11 00:23:35 -0400

    Test Nethereum dlls

---
+++ ClickMine/Assets/GetBlock.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking; 
+using Nethereum.JsonRpc.UnityClient;
+using Nethereum.Hex.HexTypes;
+using Nethereum.RPC.Eth.Blocks;
+
+public class GetBlock : MonoBehaviour {
+	public Text blockNumberText;
+	private float blockCheckRate = 3f;
+	private float lastBlockCheckTime;
+	private bool supportsProperThreads = false;
+
+	void Start () 
+	{
+		lastBlockCheckTime = 0f;
+
+		if(!supportsProperThreads)
+		{
+			StartCoroutine(CheckBlockNumber());
+		}
+	}
+
+	public IEnumerator CheckBlockNumber() 
+	{
+		while(true)
+		{ 
+			yield return new WaitForSeconds(2);
+			var blockNumberRequest = new EthBlockNumberUnityRequest("https://rinkeby.infura.io");
+			yield return blockNumberRequest.SendRequest();
+			if(blockNumberRequest.Exception == null) {
+				//blockNumberText.text = "Block: " + blockNumberRequest.Result.Value.ToString();
+				Debug.Log (blockNumberRequest.Result.Value.ToString ());
+			}
+		}
+	}
+
+	public void Update () 
+	{
+		if(supportsProperThreads)
+		{
+			lastBlockCheckTime += Time.deltaTime;
+
+			if (lastBlockCheckTime >= blockCheckRate) 
+			{
+				lastBlockCheckTime = 0f;
+				//CheckBlockNumberOnBackgroundThread();
+			}
+		}
+	}
+
+	/* 
+	public void CheckBlockNumberOnBackgroundThread()
+	{
+		Task<HexBigInteger>.Run(async () => 
+		{
+			return await web3.Eth.Blocks.GetBlockNumber.SendRequestAsync();
+
+		}).ContinueWith( blockNumber => 
+		{
+			blockNumberText.text = "Block: " + blockNumber.Result.Value.ToString();
+		}, 
+		TaskScheduler.FromCurrentSynchronizationContext());
+	}
+
+	*/
+
+}
+
+
+
