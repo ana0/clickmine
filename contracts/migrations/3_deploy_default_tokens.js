7aaff454c1e50f605bb7e77a45c98b4bc76bd112
anon <sarah.anne.friend@gmail.com>
2017-10-15 01:17:14 -0400

    Switching to js

--- contracts/migrations/3_deploy_default_tokens.js
+++ contracts/migrations/3_deploy_default_tokens.js
@@ -3,7 +3,6 @@ var Registrar = artifacts.require("./Registrar.sol");
 var Game = artifacts.require("./Game.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ClickMineToken);
   deployer.deploy(Registrar);
   deployer.deploy(Game);
 };
